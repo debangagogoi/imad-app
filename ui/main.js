522bc6fd64c86bceae19f25b5be1bc6529c64a85
debangagogoi <30667962+debangagogoi@users.noreply.github.com>
2017-08-14 18:43:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,20 @@
 console.log('Loaded!');
 var element= document.getElementById('mainText');
 element.innerHTML = 'This is the new value that we entered';
+
+
 //Move the image
 
 var dego= document.getElementById('madi');
+
+var marginLeft = 0;
+function moveright () {
+    marginLeft = marginLeft + 'px';
+    
+}
+
+
+
 dego.onclick= function() {
-    dego.style.marginleft = '100px';
+    var interval = setInterval(moveright, 50);
 };
