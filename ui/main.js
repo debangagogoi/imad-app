0b95d652524904424672c57d09b2c21971d7fe69
debangagogoi <30667962+debangagogoi@users.noreply.github.com>
2017-08-14 17:56:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
 var element= document.getElementById(mainText)
-element.innerHTML='This is the new value that we entered'
+element.innerHTML='This is the new value that we entered';
