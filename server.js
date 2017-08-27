103fced8a0303135281c485314dee88867013e88
debangagogoi <30667962+debangagogoi@users.noreply.github.com>
2017-08-27 19:00:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -18,6 +20,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
+
 app.get('/test-db', function(req, res) {
     pool.query('SELECT * FROM test', function(err, results){
         if (err){
@@ -28,3 +31,17 @@ app.get('/test-db', function(req, res) {
     })
 }
 )
+
+app.get('/ui/style.css', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
