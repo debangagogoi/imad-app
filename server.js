1da8e25180ff361549e2d5fdf795b3d1f7bdcaca
debangagogoi <30667962+debangagogoi@users.noreply.github.com>
2017-08-27 17:38:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+var config = {
+    user: 'debangagogoi',
+    database: 'debangagogoi',
+    host: 'db.imad.hasura-ap.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
